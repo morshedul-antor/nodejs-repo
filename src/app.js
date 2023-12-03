0893932f5b9059557c9c81dc33292746571ac605
morshedul-antor <antor.morshedul@gmail.com>
2023-12-03 16:00:42 +0600

    fix: error

--- src/app.js
+++ src/app.js
@@ -1,16 +1,16 @@
 const mongoose = require("mongoose");
 const express = require("express");
+
 const app = express();
+app.use(express.json());
 
 const errorResponse = require("./errors/index");
 app.use(errorResponse);
 
-app.use(express.json());
-
-mongoose.connect("mongodb://localhost:27017/mydb", {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
+mongoose
+  .connect("mongodb://localhost:27017/mydb")
+  .then(() => console.log("DB connection successfull..."))
+  .catch((err) => console.log(err));
 
 app.get("/", (reg, res) => {
   res.send({ message: "Nodejs Repository Pattern!" });
